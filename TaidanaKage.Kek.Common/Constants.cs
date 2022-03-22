a378bcb5090801eb4b082edd711edf6eb0729319
Dominik Smatana <taidana@kage.sk>
2022-03-22 17:53:45 +0100

    Cleanup

--- TaidanaKage.Kek.Common/Constants.cs
+++ TaidanaKage.Kek.Common/Constants.cs
@@ -1,35 +1,34 @@
-﻿namespace TaidanaKage.Kek.Common
+﻿namespace TaidanaKage.Kek.Common;
+
+/// <summary>
+/// Global constants.
+/// </summary>
+public static class Constants
 {
     /// <summary>
-    /// Global constants.
+    /// Developer company name.
+    /// Used also in folder structure.
     /// </summary>
-    public static class Constants
-    {
-        /// <summary>
-        /// Developer company name.
-        /// Used also in folder structure.
-        /// </summary>
-        public const string CompanyName = "Taidana Kage";
+    public const string CompanyName = "Taidana Kage";
 
-        /// <summary>
-        /// Game name.
-        /// Used also in folder structure.
-        /// </summary>
-        public const string GameName = "Kek";
+    /// <summary>
+    /// Game name.
+    /// Used also in folder structure.
+    /// </summary>
+    public const string GameName = "Kek";
 
-        /// <summary>
-        /// The name of the file containing Meta database.
-        /// </summary>
-        public const string MetaDatabaseFileName = "meta.db";
+    /// <summary>
+    /// The name of the file containing Meta database.
+    /// </summary>
+    public const string MetaDatabaseFileName = "meta.db";
 
-        /// <summary>
-        /// File name of the 32-bit WoW client executable.
-        /// </summary>
-        public const string WoWClient32ExeFileName = "WoW.exe";
+    /// <summary>
+    /// File name of the 32-bit WoW client executable.
+    /// </summary>
+    public const string WoWClient32ExeFileName = "WoW.exe";
 
-        /// <summary>
-        /// File name of the 64-bit WoW client executable.
-        /// </summary>
-        public const string WoWClient64ExeFileName = "WoW-64.exe";
-    }
+    /// <summary>
+    /// File name of the 64-bit WoW client executable.
+    /// </summary>
+    public const string WoWClient64ExeFileName = "WoW-64.exe";
 }
--- TaidanaKage.Kek.Common/WorldVersion.cs
+++ TaidanaKage.Kek.Common/WorldVersion.cs
@@ -1,35 +1,32 @@
-﻿using System;
+﻿namespace TaidanaKage.Kek.Common;
 
-namespace TaidanaKage.Kek.Common
+/// <summary>
+/// Supported world versions
+/// </summary>
+public enum WorldVersion
 {
     /// <summary>
-    /// Supported versions of WoW
+    /// Vanilla
     /// </summary>
-    public enum WorldVersion
-    {
-        /// <summary>
-        /// Vanilla
-        /// </summary>
-        W1 = 1,
+    W1 = 1,
 
-        /// <summary>
-        /// The Burning Crussade
-        /// </summary>
-        W2 = 2,
+    /// <summary>
+    /// The Burning Crussade
+    /// </summary>
+    W2 = 2,
 
-        /// <summary>
-        /// Wrath of the Lich King
-        /// </summary>
-        W3 = 3,
+    /// <summary>
+    /// Wrath of the Lich King
+    /// </summary>
+    W3 = 3,
 
-        /// <summary>
-        /// Cataclysm
-        /// </summary>
-        W4 = 4,
+    /// <summary>
+    /// Cataclysm
+    /// </summary>
+    W4 = 4,
 
-        /// <summary>
-        /// Mists of Pandaria
-        /// </summary>
-        W5 = 5
-    }
+    /// <summary>
+    /// Mists of Pandaria
+    /// </summary>
+    W5 = 5
 }
